153c7f92fc20defcc8db4dbd00b88dd72f384152
Andrew Gulik <andrewgulik@gmail.com>
2019-11-27 14:57:57 -0500

    setup.py to enable building

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='breeze',
+    version='0.1',
+    author='Andrew Gulik',
+    author_email='andrewgulik@gmail.com',
+    description='Breeze is a tool to manager AWS EC2 snapshots',
+    license='GPLv3+',
+    url='https://github.com/agulik/breeze',
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        breeze=breeze:cli
+    '''
+
+)
