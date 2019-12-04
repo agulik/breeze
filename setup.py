7d4158bdc75bc73da73cebf34213baeffd9d78f6
Andrew Gulik <andrewgulik@gmail.com>
2019-12-04 15:07:27 -0500

    fix: setup description text

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     author='Andrew Gulik',
     author_email='andrewgulik@gmail.com',
-    description='Breeze is a tool to manager AWS EC2 snapshots',
+    description='Breeze is a tool to manage AWS EC2 snapshots',
     license='GPLv3+',
     url='https://github.com/agulik/breeze',
     install_requires=[
